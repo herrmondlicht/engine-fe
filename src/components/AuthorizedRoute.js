aed18947631681738afeac4e46e28a2f7f1e61dc
Guilherme Moreira <guilherme.silva@yobetit.com>
2020-01-07 00:53:34 +0100

    fix(AuthorizedRoute): imports
    fix breaking import

--- src/components/AuthorizedRoute.js
+++ src/components/AuthorizedRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import routes from "../appRoutes";
+import routes from "../utils/appRoutes";
 
 function AuthorizedRoute(props) {
   //will do check here
