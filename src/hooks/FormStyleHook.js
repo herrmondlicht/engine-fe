431099f80e3a5c9fa777427a6b90e32fa95e9df7
Guilherme Moreira da Silva <guilherme@Mias-MacBook-Pro.local>
2020-03-28 05:55:21 +0100

    feat(formStyleHooks): add and change some form styles
    - add styles for a form divider
    - add styles for the form title

--- src/hooks/FormStyleHook.js
+++ src/hooks/FormStyleHook.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   formWrapper: {
-    padding: theme.spacing(2)
+    padding: theme.spacing(3)
   },
   paperContainer: {
     minHeight: 200,
@@ -16,6 +16,18 @@ const useStyles = makeStyles(theme => ({
   },
   buttonSubmit: {
     margin: theme.spacing(2)
+  },
+  divider: {
+    marginTop: theme.spacing(4),
+    flex: 1
+  },
+  formTitle: {
+    marginLeft: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+    }
   }
 }));
 
