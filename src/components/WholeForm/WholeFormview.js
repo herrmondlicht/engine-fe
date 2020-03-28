ebadbfdae8ca2cc37c68360d848b488b745d4177
Guilherme Moreira da Silva <guilherme@Mias-MacBook-Pro.local>
2020-03-28 06:00:40 +0100

    feat(WholeFormView): change form style

--- src/components/WholeForm/WholeFormview.js
+++ src/components/WholeForm/WholeFormview.js
@@ -4,7 +4,8 @@ import {
   Grid,
   Typography,
   Button,
-  CircularProgress
+  CircularProgress,
+  Divider
 } from "@material-ui/core";
 
 import CarFormView from "../CarForm/CarFormView";
@@ -22,15 +23,13 @@ const createWholeFormView = () =>
       <>
         <Paper variant="outlined" className={`${classes.paperContainer} ${classes.paperContainerTop}`}>
           <Grid
-            alignContent="center"
-            justify="center"
+            alignItems="center"
             container
             item
             sm={12}
             direction="column"
             className={classes.formWrapper}
           >
-            <Typography align="center" variant="h4">Dados do Cliente</Typography>
             <Grid
               container
               item
@@ -39,80 +38,88 @@ const createWholeFormView = () =>
               justify="center"
               direction="row"
             >
-              <CustomerView
-                changeFormForKey={changeCustomerFormForKey}
-                form={formsData.customerForm}
-              />
+              <Grid item sm={12}>
+                <Typography variant="h5" >Cadastrar Nova OS</Typography>
+              </Grid>
+              <FormDivider />
+              <Grid item sm={3} className={classes.formItem}>
+                <Typography variant="title">Dados do Cliente</Typography>
+              </Grid>
+              <Grid container item sm={9} spacing={2}>
+                <CustomerView
+                  changeFormForKey={changeCustomerFormForKey}
+                  form={formsData.customerForm}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </Paper >
-        <Paper variant="outlined" className={classes.paperContainer}>
-          {formsData.modelsList.length === 0
-            ? "carregando..."
-            :
-            <Grid
-              alignItems="center"
-              justify="center"
-              container
-              item
-              sm={12}
-              direction="column"
-              className={classes.formWrapper}
-            >
-              <Typography variant="h4">Dados do veículo</Typography>
-              <Grid
-                container
-                item
-                sm={12}
-                spacing={2}
-                justify="center"
-                direction="row"
-              >
-                <Grid item sm={6} xs={12}>
-                  <CarFormView
-                    modelsList={formsData.modelsList}
-                    form={formsData.carForm}
-                    changeFormForKey={changeCarFormForKey} />
-                </Grid>
-                <Grid item sm={6} xs={12}>
-                  <CustomerCarFormView
-                    changeFormForKey={changeCustomerCarFormForKey}
-                    form={formsData.customerCarForm}
-                  />
-                </Grid>
-              </Grid>
-              <Grid
-                container
-                justify="center"
-                alignItems="center"
-                className={classes.formWrapper}
-              >
-                {error && <span style={{ margin: 10, color: "red" }}>{error}</span>}
-              </Grid>
-              <Grid
-                item
-                container
-                xs={12}
-                justify="flex-end"
-                className={classes.formWrapper}
-              >
-                <Button
-                  variant="contained"
-                  color="primary"
-                  type="submit"
-                  data-testid="LoginForm_button"
-                  disabled={isLoading}
-                  onClick={sendForm}
+            <FormDivider />
+            {formsData.modelsList.length === 0
+              ? "carregando..."
+              :
+              <>
+                <Grid
+                  container
+                  item
+                  sm={12}
+                  spacing={2}
+                  justify="center"
+                  direction="row"
+                  className={classes.formItem}
                 >
-                  {isLoading ? <CircularProgress size={24} /> : "Confirmar dados"}
-                </Button>
-              </Grid>
-            </Grid>}
+                  <Grid item sm={3} className={classes.formItem}>
+                    <Typography variant="title">Dados do Veículo</Typography>
+                  </Grid>
+                  <Grid container item sm={9} spacing={2}>
+                    <CarFormView
+                      modelsList={formsData.modelsList}
+                      form={formsData.carForm}
+                      changeFormForKey={changeCarFormForKey} />
+                    <CustomerCarFormView
+                      changeFormForKey={changeCustomerCarFormForKey}
+                      form={formsData.customerCarForm}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid
+                  container
+                  justify="center"
+                  alignItems="center"
+                  className={classes.formWrapper}
+                >
+                  {error && <span style={{ margin: 10, color: "red" }}>{error}</span>}
+                </Grid>
+                <Grid
+                  item
+                  container
+                  xs={12}
+                  justify="flex-end"
+                  className={classes.formWrapper}
+                >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    data-testid="LoginForm_button"
+                    disabled={isLoading}
+                    onClick={sendForm}
+                  >
+                    {isLoading ? <CircularProgress size={24} /> : "Confirmar dados"}
+                  </Button>
+                </Grid>
+              </>}
+          </Grid>
         </Paper >
       </>
     )
   }
 
-
+function FormDivider() {
+  const classes = useStyles()
+  return (
+    <Grid container item xs={12} className={classes.formItem}>
+      <Divider variant="fullWidth" className={classes.divider} />
+    </Grid>
+  )
+}
 
 export default createWholeFormView()
