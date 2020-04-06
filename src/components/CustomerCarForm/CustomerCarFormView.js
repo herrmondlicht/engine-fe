f4e5391007cc4ff97ea763e490952650d2465064
Guilherme Moreira <guilhermegms.silva@gmail.com>
2020-04-06 18:31:11 +0200

    fix(customerCarForm): license plate not changing

--- src/components/CustomerCarForm/CustomerCarFormView.js
+++ src/components/CustomerCarForm/CustomerCarFormView.js
@@ -1,38 +1,33 @@
-
 import React from "react";
-import {
-  Grid,
-  TextField,
-} from "@material-ui/core";
+import { Grid, TextField } from "@material-ui/core";
 
-import useStyles from "../../hooks/FormStyleHook"
+import useStyles from "../../hooks/FormStyleHook";
 
 export const createAddCustomerCarView = () =>
   function AddCustomerCarView({ changeFormForKey, form }) {
-    const classes = useStyles();
-
     function licenseKeyDown(e) {
-      e.persist()
+      e.persist();
       const isBackspace = e.keyCode === 8;
-
-      !isBackspace &&
-        changeFormForKey("licensePlate", true)(applyLicensePlateMask(form.licensePlate));
+      if (!isBackspace) {
+        e.target.value = applyLicensePlateMask(form.licensePlate);
+        changeFormForKey("licensePlate")(e);
+      }
     }
 
     function applyLicensePlateMask(value) {
-      if (value)
-        return value.replace(/^.{3}(?!-)/g, `${value.toUpperCase()}-`)
-      return ""
+      if (value) return value.replace(/^.{3}(?!-)/g, `${value.toUpperCase()}-`);
+      return "";
     }
 
     function licenseOnChange(e) {
-      e.persist()
-      changeFormForKey("licensePlate", true)(e?.target?.value.toUpperCase())
+      e.persist();
+      e.target.value = e?.target?.value.toUpperCase();
+      changeFormForKey("licensePlate")(e);
     }
 
     return (
       <>
-        <Grid item xs={12} sm={4} className={classes.formItem}>
+        <Grid item xs={12} sm={4}>
           <TextField
             onChange={licenseOnChange}
             onKeyDown={licenseKeyDown}
@@ -40,34 +35,31 @@ export const createAddCustomerCarView = () =>
             size="small"
             label={"Placa"}
             variant="outlined"
-            inputProps={
-              { maxLength: "8" }
-            }
-            fullWidth />
-
+            inputProps={{ maxLength: "8" }}
+            fullWidth
+          />
         </Grid>
-        <Grid item xs={12} sm={4} className={classes.formItem}>
+        <Grid item xs={12} sm={4}>
           <TextField
             onChange={changeFormForKey("color")}
             size="small"
             label={"Cor"}
             variant="outlined"
-            fullWidth />
+            fullWidth
+          />
         </Grid>
-        <Grid item xs={12} sm={4} className={classes.formItem}>
+        <Grid item xs={12} sm={4}>
           <TextField
             onChange={changeFormForKey("displacement")}
             size="small"
             label={"Cilindradas"}
-            inputProps={
-              { maxLength: "3" }
-            }
+            inputProps={{ maxLength: "3" }}
             variant="outlined"
-            fullWidth />
+            fullWidth
+          />
         </Grid>
       </>
-    )
-  }
+    );
+  };
 
-
-export default createAddCustomerCarView()
+export default createAddCustomerCarView();
