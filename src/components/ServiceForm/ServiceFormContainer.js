f98993aa259e1bdf44f36cd360608d7eed15f925
Guilherme Moreira <guilhermegms.silva@gmail.com>
2020-04-07 11:57:53 +0200

    refactor(ServiceFormContainer): render
    - add financial details
    - add Paper wrapper

--- src/components/ServiceForm/ServiceFormContainer.js
+++ src/components/ServiceForm/ServiceFormContainer.js
@@ -1,13 +1,27 @@
 import React from "react";
 import CustomerFormsContainer from "../CustomerForms/CustomerFormsContainer";
-import ServiceItems from "./ServiceItems/ServiceItems";
+import ServiceItemsContainer from "./ServiceItems/ServiceItemsContainer";
+import PaperWithTitle from "../Common/PaperWithTitle";
+import useStyles from "../../hooks/FormStyleHook";
+import FinancialDetails from "./FinancialDetails/FinancialDetails";
 
 export const createServiceFormContainer = () =>
   function ServiceFormContainer() {
+    const classes = useStyles();
     return (
       <>
-        <CustomerFormsContainer />
-        <ServiceItems />
+        <PaperWithTitle
+          title="Cadastrar Nova OS"
+          paperClassNames={classes.paperContainerTop}
+        >
+          <CustomerFormsContainer />
+        </PaperWithTitle>
+        <PaperWithTitle title="Itens do Serviço">
+          <ServiceItemsContainer />
+        </PaperWithTitle>
+        <PaperWithTitle title="Pagamento">
+          <FinancialDetails />
+        </PaperWithTitle>
       </>
     );
   };
