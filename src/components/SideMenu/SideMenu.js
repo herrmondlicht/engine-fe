df836aafe76011cfb97fd54904df5be98300b515
Guilherme Silva <guilherme.silva@redacreltd.com>
2020-05-02 19:14:56 +0200

    feat: add new application routes

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,51 +1,51 @@
 import React, { useState } from "react";
-import { makeStyles, Grid, Paper, IconButton, BottomNavigation, BottomNavigationAction, isWidthDown, withWidth, isWidthUp } from "@material-ui/core";
+import {
+  makeStyles,
+  Grid,
+  Paper,
+  IconButton,
+  BottomNavigation,
+  BottomNavigationAction,
+  isWidthDown,
+  withWidth,
+  isWidthUp,
+} from "@material-ui/core";
 import { NoteAdd } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   sideMenuContainer: {
     height: "100%",
     width: "100%",
     position: "relative",
     [theme.breakpoints.up("sm")]: {
-      marginTop: theme.spacing(6)
-    }
-
+      marginTop: theme.spacing(6),
+    },
   },
   sideMenu: {
     minHeight: theme.spacing(10),
     padding: `${theme.spacing(2)}px 0`,
     width: `100%`,
-  }
+  },
 }));
-
 
 function SideMenu({ setSelectedItem, selectedItem }) {
   const classes = useStyles();
 
-  const changeSelectedItem = (name) => () => setSelectedItem(name)
+  const changeSelectedItem = (name) => () => setSelectedItem(name);
 
   return (
-    <Grid
-      container
-      direction="column"
-      className={classes.sideMenuContainer}
-    >
+    <Grid container direction="column" className={classes.sideMenuContainer}>
       <Paper className={classes.sideMenu}>
-        <Grid container
-          justify="center"
-          alignItems="center"
-          direction="column"
-        >
+        <Grid container justify="center" alignItems="center" direction="column">
           <Grid item container justify="center">
             <IconButton
               aria-label="delete"
               width={50}
               component={Link}
-              to="/new-service"
-              onClick={changeSelectedItem("new-service")}
-              color={selectedItem === "new-service" ? "secondary" : "default"}
+              to="/customers/new"
+              onClick={changeSelectedItem("newCustomer")}
+              color={selectedItem === "newCustomer" ? "secondary" : "default"}
             >
               <NoteAdd />
             </IconButton>
@@ -55,7 +55,7 @@ function SideMenu({ setSelectedItem, selectedItem }) {
               aria-label="delete"
               width={50}
               component={Link}
-              to="/new-service"
+              to="/customers"
               onClick={changeSelectedItem("new-service2")}
               color={selectedItem === "new-service2" ? "secondary" : "default"}
             >
@@ -67,7 +67,19 @@ function SideMenu({ setSelectedItem, selectedItem }) {
               aria-label="delete"
               width={50}
               component={Link}
-              to="/new-service"
+              to="/customers/1"
+              onClick={changeSelectedItem("new-service3")}
+              color={selectedItem === "new-service3" ? "secondary" : "default"}
+            >
+              <NoteAdd />
+            </IconButton>
+          </Grid>
+          <Grid item container justify="center">
+            <IconButton
+              aria-label="delete"
+              width={50}
+              component={Link}
+              to="/services/null/new"
               onClick={changeSelectedItem("new-service3")}
               color={selectedItem === "new-service3" ? "secondary" : "default"}
             >
@@ -85,27 +97,27 @@ export const createMenu = () => {
     const [selectedItem, setSelectedItem] = useState();
     return (
       <>
-        {isWidthDown("sm", width) &&
+        {isWidthDown("sm", width) && (
           <BottomNavigation
             value={selectedItem}
             onChange={(event, newValue) => {
               setSelectedItem(newValue);
             }}
-            >
+          >
             <BottomNavigationAction label="Nova OS" icon={<NoteAdd />} />
             <BottomNavigationAction label="Nova OS" icon={<NoteAdd />} />
             <BottomNavigationAction label="Nova OS" icon={<NoteAdd />} />
           </BottomNavigation>
-        }
-        {isWidthUp("md", width) &&
+        )}
+        {isWidthUp("md", width) && (
           <SideMenu
             selectedItem={selectedItem}
-            setSelectedItem={setSelectedItem} />
-        }
-
+            setSelectedItem={setSelectedItem}
+          />
+        )}
       </>
-    )
-  }
+    );
+  };
 
   return Menu;
 };
--- src/utils/appRoutes.js
+++ src/utils/appRoutes.js
@@ -1,13 +1,44 @@
+import React from "react";
+//temporary /\/\/\/\/\/\
 import ServiceFormContainer from "../components/ServiceForm/ServiceFormContainer";
+import CustomerFormsContainerWithPaper from "../components/CustomerForms/CustomerFormsContainerWithPaper";
 
 const routes = {
   newService: {
     name: "New Service",
-    path: "new-service",
+    path: "services/:customer_car/new",
     Component: ServiceFormContainer,
     isLocked: true,
     exact: true,
-  }
+  },
+  customers: {
+    name: "Customers",
+    path: "customers",
+    Component: () => <div>TODO: Customer List</div>,
+    isLocked: true,
+    exact: true,
+  },
+  newCustomer: {
+    name: "New Customer",
+    path: "customers/new",
+    Component: CustomerFormsContainerWithPaper,
+    isLocked: true,
+    exact: true,
+  },
+  customer: {
+    name: "Customer",
+    path: "customers/:id",
+    Component: () => <div>TODO: Customer</div>,
+    isLocked: true,
+    exact: true,
+  },
+  customerServices: {
+    name: "Customer Services",
+    path: "customers/:id/services",
+    Component: () => <div>TODO: Customer Services</div>,
+    isLocked: true,
+    exact: true,
+  },
 };
 
 export default routes;
