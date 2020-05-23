01b793847e4625fcabb69d72d24d2b4da16f72e7
Guilherme Silva <guilherme.silva@redacreltd.com>
2020-05-24 00:12:11 +0200

    feat(SideMenu): improves sidemenu

---
+++ src/components/CustomerCarList/CustomerCarList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import engineAPI from "../../utils/engineAPI/engineAPI";
+
+export const createCustomerCarList = ({ engineAPI }) =>
+  function CustomerCarList() {
+    return <div>Tada!</div>;
+  };
+
+export default createCustomerCarList({ engineAPI });
