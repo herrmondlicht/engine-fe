4267a3b06c895783353789dcdbbf009fc0a9e121
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-05-04 00:12:04 +0200

    feat(CustomerForm): add form validators

--- src/components/CustomerForms/CustomerForm.js
+++ src/components/CustomerForms/CustomerForm.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
 import { Input, Card, Button } from "ui-fragments";
 
+const schema = yup.object().shape({
+  CPF: yup.string(),
+  address: yup.string(),
+  email: yup.string(),
+  name: yup.string().required(),
+  phone: yup.string(),
+});
+
+const useValidator = (validator) => {
+  const [errors, setErrors] = useState({});
+  const validate = useCallback(
+    (object) => {
+      try {
+        return validator.validateSync(object);
+      } catch (e) {
+        setErrors({ [e.path]: e.errors[0] });
+      }
+    },
+    [validator]
+  );
+
+  return { validate, errors };
+};
+
 const CustomerForm = () => {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors, validate } = useValidator(schema);
+
+  const submit = (data) => {
+    console.log(data);
+    validate(data);
+  };
   return (
-    <div>
+    <form onSubmit={handleSubmit(submit)}>
       <Card>
         <p className="text-sm text-gray-600">Nova OS</p>
         <div className="my-3">
@@ -12,19 +46,19 @@ const CustomerForm = () => {
         <div className="mt-3">
           <div className="flex">
             <div className="w-1/3">
-              <Input fw label="CPF" placeholder="CPF" />
+              <Input fw label="CPF" placeholder="CPF" {...register("CPF")} error={errors.cpf} />
             </div>
             <div className="ml-12 w-full">
-              <Input fw label="Endereço" placeholder="Endereço" />
+              <Input fw label="Endereço" placeholder="Endereço" error={errors.address} />
             </div>
           </div>
           <div className="flex mt-3">
             <div className="w-1/3">
-              <Input fw label="Email" placeholder="Email" />
+              <Input fw label="Email" placeholder="Email" error={errors.email} />
             </div>
             <div className="ml-12 w-full flex">
               <div className="w-full">
-                <Input fw label="Nome" placeholder="Nome" />
+                <Input fw label="Nome" placeholder="Nome" error={errors.name} />
               </div>
               <div className="ml-12 w-full">
                 <Input fw label="Telefone" placeholder="Telefone" />
@@ -36,7 +70,7 @@ const CustomerForm = () => {
           </div>
         </div>
       </Card>
-    </div>
+    </form>
   );
 };
 
