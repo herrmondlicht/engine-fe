435a41825fb7d4cdace5d6b47c137cde88bdc2c6
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-05-11 00:48:33 +0200

    feat(Button): add loader to button

--- src/ui-fragments/Button/Button.js
+++ src/ui-fragments/Button/Button.js
@@ -1,8 +1,9 @@
 import React, { useMemo } from "react";
 import propTypes from "prop-types";
+import Loader from "react-loader-spinner";
 
 const Button = React.forwardRef(
-  ({ onClick, children, variant, size, fw }, ref) => {
+  ({ onClick, children, variant, size, fw, showLoader }, ref) => {
     const sizeClasses = useMemo(() => {
       switch (size) {
         case "small":
@@ -34,9 +35,18 @@ const Button = React.forwardRef(
         onClick={onClick ? onClick : undefined}
         className={`${sizeClasses} ${variantClasses} ${
           fw ? "w-full" : ""
-        } rounded-full text-white font-semibold`}
+        } rounded-full text-white font-semibold relative`}
       >
-        {children}
+        <div className={`transition duration-400 ${showLoader ? "opacity-0" : "opacity-100"}`}>
+          {children}
+        </div>
+        <div
+          className={`absolute z-10 w-full h-full flex justify-center items-center top-0 right-0 transition duration-400 ${
+            showLoader ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <Loader type="TailSpin" height={30} width={30} color="#FFF" />
+        </div>
       </button>
     );
   }
@@ -47,5 +57,7 @@ Button.propTypes = {
   onClick: propTypes.func,
   variant: propTypes.string,
   size: propTypes.string,
+  fw: propTypes.bool,
+  showLoader: propTypes.bool,
 };
 export { Button };
