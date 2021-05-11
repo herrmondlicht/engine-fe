a3750c3a87db63161deb4ae877c0f0a734a788da
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-05-11 23:42:47 +0200

    feat(Button): add icon to variants

--- src/ui-fragments/Button/Button.js
+++ src/ui-fragments/Button/Button.js
@@ -1,56 +1,94 @@
 import React, { useMemo } from "react";
 import propTypes from "prop-types";
 import Loader from "react-loader-spinner";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/solid";
 
-const Button = React.forwardRef(
-  ({ onClick, children, variant, size, fw, showLoader }, ref) => {
-    const sizeClasses = useMemo(() => {
-      switch (size) {
-        case "small":
-          return ["p-2", "px-4", "text-sm"].join(" ");
-        case "big":
-          return ["p-4", "px-6", "text-lg"].join(" ");
-        default:
-          return ["p-3", "px-5", "text-md"].join(" ");
-      }
-    }, [size]);
+const Button = (
+  { onClick, children, variant = "primary", size, fw, disabled, showLoader },
+  ref
+) => {
+  const sizeClasses = useMemo(() => {
+    switch (size) {
+      case "small":
+        return ["p-2", "px-4", "text-sm"].join(" ");
+      case "big":
+        return ["p-4", "px-6", "text-lg"].join(" ");
+      default:
+        return ["p-3", "px-5", "text-md"].join(" ");
+    }
+  }, [size]);
 
-    const variantClasses = useMemo(() => {
-      const color = `${variant ? variant : "primary"}`;
-      const variantColor = `${color}-0`;
-      return [
-        `bg-${variantColor}`,
-        "focus:ring-2",
-        "focus:border-transparent",
-        "border-2",
-        `focus:ring-${variantColor}`,
-        `hover:bg-${color}-1`,
-        "transition duration-300",
-      ].join(" ");
-    }, [variant]);
+  const variantClasses = useMemo(() => {
+    const variantColor = `${variant}-0`;
+    return [
+      `bg-${variantColor}`,
+      "focus:ring-2",
+      "focus:border-transparent",
+      "border-2",
+      `focus:ring-${variantColor}`,
+      `hover:bg-${variant}-1`,
+      "transition duration-300",
+    ].join(" ");
+  }, [variant]);
 
-    return (
-      <button
-        ref={ref}
-        onClick={onClick ? onClick : undefined}
-        className={`${sizeClasses} ${variantClasses} ${
-          fw ? "w-full" : ""
-        } rounded-full text-white font-semibold relative`}
+  return (
+    <button
+      disabled={disabled}
+      ref={ref}
+      onClick={onClick ? onClick : undefined}
+      className={`${sizeClasses} ${variantClasses} ${
+        fw ? "w-full" : ""
+      } rounded-full text-white font-semibold`}
+    >
+      <ButtonContent showLoader={showLoader} variant={variant}>
+        {children}
+      </ButtonContent>
+    </button>
+  );
+};
+
+const ButtonContent = ({ children, showLoader, variant }) => {
+  const variantIcon = useMemo(() => {
+    const sizeClasses = "h-7 w-7"
+    switch (variant) {
+      case "success":
+        return <CheckCircleIcon className={sizeClasses} />;
+      case "error":
+        return <XCircleIcon className={sizeClasses} />;
+      default:
+        return null;
+    }
+  }, [variant]);
+  return (
+    <div className="relative w-full h-full flex justify-center items-center">
+      <div
+        className={`transition duration-400 ${
+          showLoader || variant === "success" ? "opacity-0" : "opacity-100"
+        }`}
       >
-        <div className={`transition duration-400 ${showLoader ? "opacity-0" : "opacity-100"}`}>
-          {children}
-        </div>
+        {children}
+      </div>
+      <div
+        className={`absolute z-10 transition duration-400 ${
+          showLoader ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <Loader type="TailSpin" height={30} width={30} color="#FFF" />
+      </div>
+      {!showLoader && (
         <div
-          className={`absolute z-10 w-full h-full flex justify-center items-center top-0 right-0 transition duration-400 ${
-            showLoader ? "opacity-100" : "opacity-0"
+          className={`absolute z-10 transition duration-400 ${
+            variantIcon ? "opacity-100" : "opacity-0"
           }`}
         >
-          <Loader type="TailSpin" height={30} width={30} color="#FFF" />
+          {variantIcon}
         </div>
-      </button>
-    );
-  }
-);
+      )}
+    </div>
+  );
+};
+
+const ButtonFR = React.forwardRef(Button);
 
 Button.propTypes = {
   children: propTypes.any,
@@ -59,5 +97,6 @@ Button.propTypes = {
   size: propTypes.string,
   fw: propTypes.bool,
   showLoader: propTypes.bool,
+  disabled: propTypes.bool,
 };
-export { Button };
+export { ButtonFR as Button };
