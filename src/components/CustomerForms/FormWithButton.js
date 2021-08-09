8c7106d1cb1e5b88112b247b8e7d6d2f855007cc
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-08-10 01:23:37 +0200

    feat(FormWithButton): add component to abstract forms

---
+++ src/components/CustomerForms/FormWithButton.js
@@ -0,0 +1,51 @@
+import { useCustomForm } from "hooks/useCustomForm";
+import React, { useState } from "react";
+
+import { Button } from "ui-fragments";
+
+const FormWithButton = ({
+  preloadedData,
+  formValidationSchema,
+  onFormSubmit,
+  title,
+  description,
+  Form,
+  buttonConfig: { titleWhenEditing, defaultTitle },
+}) => {
+  const {
+    formMethods: { register, handleSubmit, formState, reset },
+    validationMethods: { errors, validate },
+  } = useCustomForm({ schema: formValidationSchema, preloadedData });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { isDirty } = formState;
+  const submit = async (data) => {
+    if (!validate(data)) return;
+    setIsLoading(true);
+    reset(data);
+    await onFormSubmit(data);
+    setIsLoading(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <p className="text-sm text-gray-600">{title}</p>
+      <div className="my-3">
+        <p className="text-2xl font-bold">{description}</p>
+      </div>
+      <div className="mt-3">
+        <Form register={register} errors={errors} />
+      </div>
+      <div className="flex justify-end">
+        <Button
+          showLoader={isLoading}
+          variant={preloadedData && !isDirty ? "success" : "primary"}
+        >
+          {preloadedData ? titleWhenEditing : defaultTitle}
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export { FormWithButton };
