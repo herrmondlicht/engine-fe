ff013d8d154d269a66453c17550b091aa6e43957
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-09-08 16:11:48 +0200

    feat(useNotification): add extensions for error and success notification

--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -1,9 +1,37 @@
 import { useContext } from "react";
 
-import { NotificationContext } from "context";
+import {
+  NotificationContext,
+  NOTIFICATION_DURATION,
+  NOTIFICATION_TYPES,
+} from "context";
 
 export const useNotification = () => {
   const { showNotification, removeNotification } =
     useContext(NotificationContext);
-  return { showNotification, removeNotification };
+
+  const showErrorNotification = ({ id, title, message }) =>
+    showNotification({
+      duration: NOTIFICATION_DURATION.LONG,
+      type: NOTIFICATION_TYPES.ERROR,
+      id,
+      title: title ?? "Opa!",
+      message: message ?? "Algo errado aconteceu",
+    });
+
+  const showSuccessNotification = ({ id, title, message }) =>
+    showNotification({
+      duration: NOTIFICATION_DURATION.SHORT,
+      type: NOTIFICATION_TYPES.SUCCESS,
+      id,
+      title: title ?? "Oba!",
+      message: message ?? "Ação feita com sucesso",
+    });
+
+  return {
+    showNotification,
+    removeNotification,
+    showErrorNotification,
+    showSuccessNotification,
+  };
 };
