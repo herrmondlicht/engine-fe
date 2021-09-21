dab86e7f769f6f86723acc17a3fa6969bb0f89a0
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-09-21 17:19:36 +0200

    fix(ui-fragments/Labels): increase label size

--- src/ui-fragments/Typography/Label.js
+++ src/ui-fragments/Typography/Label.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Label = ({ children, error }) => (
-  <label className={`text-sm ${error ? "text-error-0" : "text-gray-500"}`}>
+  <label className={`text-md ${error ? "text-error-0" : "text-gray-500"}`}>
     {children}
   </label>
 );
