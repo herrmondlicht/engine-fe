e8e30bbe5a72c0965ad0d4047952cb13aa0b9139
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-10-04 00:39:26 +0200

    fix(app): remove usused files and forms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as RegisterForm } from "./RegisterForm/RegisterForm";
 export { default as ServicePage } from "./ServicePage/ServicePage";
+export { default as HomePage } from "./HomePage/HomePage";
+export { default as LoginPage } from "./LoginPage";
