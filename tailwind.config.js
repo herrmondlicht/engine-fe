2d80d39d87e9e79911fea078466e857fffb1d3f7
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-10-04 00:10:28 +0200

    chore(tailwind): add purge to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,6 @@ const { theme } = require("./src/twtheme");
 
 module.exports = {
   theme,
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   plugins: [require("tailwindcss"), require("autoprefixer")],
 };
