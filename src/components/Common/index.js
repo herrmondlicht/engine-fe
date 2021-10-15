5e4341e290fd6e85515cadfb6e2613a552a44541
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-10-15 21:46:51 +0200

    fix(PaperWithTitle): remove unused component

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,5 +1,2 @@
-export { default as Autocomplete } from "./Autocomplete";
-export { default as CurrencyInput } from "./CurrencyInput";
-export { default as PaperWithTitle } from "./PaperWithTitle";
 export { default as SearchBar } from "./SearchBar";
 export * from "./PageTitle";
