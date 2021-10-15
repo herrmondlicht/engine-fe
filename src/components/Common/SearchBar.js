4ad7053d4e7ebb9ecea4b98a9baaf9b7c31227c6
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-10-15 22:48:09 +0200

    fix(SearchBar): replace obsolete components

--- src/components/Common/SearchBar.js
+++ src/components/Common/SearchBar.js
@@ -1,34 +1,32 @@
 import React from "react";
-import { TextField, IconButton, Button } from "@material-ui/core";
-import { HiSearch as SearchIcon, HiUserAdd as AddCircle } from "react-icons/hi";
+import SearchIcon from "@heroicons/react/solid/SearchIcon";
+import AddCircle from "@heroicons/react/solid/UserAddIcon";
+import { Input, Button, BUTTON_VARIANTS } from "ui-fragments";
 
 const createSearchBar = () =>
   function SearchBar({ setResearch, addAction }) {
     return (
       <div className="flex flex-col sm:flex-row items-center">
-        <div className="w-full sm:w-1/2 flex items-center">
-          <TextField
-            onChange={e => setResearch(e.target.value)}
-            label="Pesquisar"
-            variant="outlined"
-            size="small"
-            type="text"
-            className="w-full"
-          />
-          <div className="w-10 ml-1">
-            <IconButton variant="outlined" color="primary">
-              <SearchIcon />
-            </IconButton>
+        <div className="w-full sm:w-1/2 flex items-end">
+          <div className="w-full">
+            <Input
+              onChange={e => setResearch(e.target.value)}
+              label="Pesquisar"
+              size="small"
+              type="text"
+              fw
+            />
           </div>
+          <Button variant={BUTTON_VARIANTS.GHOST}>
+            <SearchIcon className="text-primary-0 w-7 h-7" />
+          </Button>
         </div>
         <div className="sm:w-1/2 w-full flex justify-center sm:justify-end">
-          <Button
-            onClick={addAction}
-            color="primary"
-            variant="contained"
-            startIcon={<AddCircle />}
-          >
-            Adicionar
+          <Button onClick={addAction}>
+            <div className="flex gap-3">
+              <AddCircle className="text-white w-5 h-5" />
+              Adicionar
+            </div>
           </Button>
         </div>
       </div>
