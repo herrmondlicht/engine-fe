fd82aa3a4433f312109bb32fa0057387c3c6e3d2
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-10-17 04:35:53 +0200

    chore(app): remove material-ui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
 import { CombinedFormsProvider } from "context";
 import { NotificationProvider } from "context/NotificationContext";
 import { HomePage, LoginPage } from "pages";
 
-import theme from "./theme";
-
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <NotificationProvider>
-        <Router>
-          <Switch>
-            <Route path="/login" exact>
-              <LoginPage />
-            </Route>
-            <Route path="/">
-              <CombinedFormsProvider value={{}}>
-                <HomePage />
-              </CombinedFormsProvider>
-            </Route>
-          </Switch>
-        </Router>
-      </NotificationProvider>
-    </ThemeProvider>
+    <NotificationProvider>
+      <Router>
+        <Switch>
+          <Route path="/login" exact>
+            <LoginPage />
+          </Route>
+          <Route path="/">
+            <CombinedFormsProvider value={{}}>
+              <HomePage />
+            </CombinedFormsProvider>
+          </Route>
+        </Switch>
+      </Router>
+    </NotificationProvider>
   );
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,6 @@ export * from "./storage/storageAPI";
 export * from "./engineAPI/APIRoutes";
 export * from "./engineAPI/engineAPI";
 export * from "./formValidator";
-export * from "./errorMessages";
 export * from "./apiFieldTranslator/apiFieldsTranslator";
 export * from "./currency";
 export * from "./misc";
