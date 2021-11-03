209a00242dbba30bc8b02764fd25a0021b208e78
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-11-04 00:35:59 +0100

    Feat/implement react table (#31)
    * feat(Table): add base component
    
    * chore(package): add react-table
    
    * feat(Table): implement react table
    
    * feat(CustomerCarList): implement Table component
    
    * fix(Table): width responsive

---
+++ src/components/Table/Table.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useTable } from "react-table";
+
+const Table = ({ columns, data, onRowClick }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+  return (
+    <div className="w-full overflow-auto">
+      <table {...getTableProps()} className="h-full w-full table-auto">
+        <thead>
+          {headerGroups.map((headerGroup, index) => (
+            <tr key={index} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  className="py-5 text-left"
+                  key={column.id}
+                  {...column.getHeaderProps()}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr
+                key={i}
+                {...row.getRowProps()}
+                className={`hover:bg-gray-300 cursor-pointer ${
+                  i % 2 ? "bg-gray-100" : ""
+                }`}
+                onClick={() => onRowClick?.(row.original)}
+              >
+                {row.cells.map((cell, j) => {
+                  return (
+                    <td key={j} {...cell.getCellProps()} className="py-1">
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export { Table };
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export * from "./CustomerForms";
 export * from "./Login";
 export * from "./ServiceForm";
 export * from "./CustomerDetails";
+export * from "./Table";
 export { default as CustomerCarList } from "./CustomerCarList/CustomerCarList";
 export { default as SideMenu } from "./SideMenu/SideMenu";
 export { default as ConfirmDeleteModal } from "./Modals/ConfirmDeleteModal";
