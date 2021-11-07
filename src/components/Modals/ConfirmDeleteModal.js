ee743e497d8bef8d985c6d0d1837efe17e5131f3
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-11-07 04:08:43 +0100

    feat(ConfirmDeleteModal): add a more generic message and a loader when saving

--- src/components/Modals/ConfirmDeleteModal.js
+++ src/components/Modals/ConfirmDeleteModal.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Modal, Button, Text, BUTTON_VARIANTS } from "ui-fragments";
 
-function ConfirmDeleteModal({ handleClose, isOpen, onConfirmationClick }) {
+function ConfirmDeleteModal({
+  handleClose,
+  isOpen,
+  onConfirmationClick,
+  isLoading,
+}) {
   return (
     <Modal isOpen={isOpen} onClose={handleClose} title="Excluir">
       <div>
         <div>
           <Text>
-            Tem certeza que quer excluir o cliente? Essa ação não poderá ser
-            revertida
+            Tem certeza que quer excluir? Essa ação não poderá ser revertida
           </Text>
         </div>
         <div className="mt-4 flex gap-3 justify-end">
@@ -19,7 +23,11 @@ function ConfirmDeleteModal({ handleClose, isOpen, onConfirmationClick }) {
           >
             Cancelar
           </Button>
-          <Button onClick={onConfirmationClick} variant={BUTTON_VARIANTS.GHOST}>
+          <Button
+            onClick={onConfirmationClick}
+            variant={BUTTON_VARIANTS.GHOST}
+            showLoader={isLoading}
+          >
             Excluir
           </Button>
         </div>
