642344f0193b504ad7650fe86325fde31b4dd3ee
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-11-07 04:14:00 +0100

    feat(FormWithButton): add option to ignore variant changes

--- src/components/CustomerForms/FormWithButton.js
+++ src/components/CustomerForms/FormWithButton.js
@@ -1,6 +1,6 @@
 import { PageTitle } from "components";
 import { useCustomForm } from "hooks";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import { Button } from "ui-fragments";
 
@@ -11,7 +11,7 @@ const FormWithButton = ({
   title,
   description,
   Form,
-  buttonConfig: { titleWhenEditing, defaultTitle },
+  buttonConfig: { titleWhenEditing, defaultTitle, ignoreVariantChanges },
 }) => {
   const {
     formMethods: { handleSubmit, formState, reset, ...formMethods },
@@ -29,6 +29,11 @@ const FormWithButton = ({
     setIsLoading(false);
   };
 
+  const showSuccessVariant = useMemo(
+    () => isValidPreloadData && !isDirty,
+    [isDirty, isValidPreloadData]
+  );
+
   return (
     <form onSubmit={handleSubmit(submit)}>
       <PageTitle title={title} description={description} />
@@ -38,7 +43,9 @@ const FormWithButton = ({
       <div className="flex justify-end mt-3 md:mt-8">
         <Button
           showLoader={isLoading}
-          variant={isValidPreloadData && !isDirty ? "success" : "primary"}
+          variant={
+            showSuccessVariant && !ignoreVariantChanges ? "success" : "primary"
+          }
         >
           {isValidPreloadData ? titleWhenEditing : defaultTitle}
         </Button>
