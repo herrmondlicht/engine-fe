56b6487595c47f13b3cb85f8d153a3eae307fd90
Guilherme Silva <guilhermegms.silva@gmail.com>
2021-11-12 00:44:38 +0100

    feat(AddServiceButton): add component

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export * from "./Login";
 export * from "./ServiceForm";
 export * from "./CustomerDetails";
 export * from "./Table";
+export * from "./AddServiceButton/AddServiceButton";
 export { default as SideMenu } from "./SideMenu/SideMenu";
 export { default as ConfirmDeleteModal } from "./Modals/ConfirmDeleteModal";
 export { default as AuthorizedRoute } from "./AuthorizedRoute";
