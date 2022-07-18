0320af3a9a2854f2aba875e03527ddc943d466f0
Guilherme Silva <guilhermegms.silva@gmail.com>
2022-07-18 16:59:19 +0200

    fix(app): improve SWR update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NotificationProvider } from "context";
 import { HomePage, LoginPage } from "pages";
+import { SWRConfig } from "swr";
 
 function App() {
   return (
-    <NotificationProvider>
-      <Router>
-        <Switch>
-          <Route path="/login" exact>
-            <LoginPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
-    </NotificationProvider>
+    <SWRConfig value={{ revalidateOnFocus: false }}>
+      <NotificationProvider>
+        <Router>
+          <Switch>
+            <Route path="/login" exact>
+              <LoginPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </NotificationProvider>
+    </SWRConfig>
   );
 }
 
