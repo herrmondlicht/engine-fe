4949bc59a175f8f50096aa46b4893e7b2beb6b32
Guilherme Silva <guilhermegms.silva@gmail.com>
2022-10-19 14:41:21 +0200

    test(ScreenLoader & Alert): add missing tests

--- src/ui-fragments/Alert/Alert.test.js
+++ src/ui-fragments/Alert/Alert.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, act } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { stub, assert } from "sinon";
 import { Alert } from "./Alert";
 jest.useFakeTimers();
--- src/ui-fragments/ScreenLoader/ScreenLoader.js
+++ src/ui-fragments/ScreenLoader/ScreenLoader.js
@@ -25,6 +25,7 @@ export const ScreenLoader = ({ children, radius = false, isLoading }) => {
     <div className="h-full w-full relative">
       {showLoader && (
         <div
+          data-testid="ScreenLoader_loader"
           className={`absolute h-full w-full bg-white transition-duration-400 flex justify-center items-center ${
             isLoading ? "opacity-70 z-10" : "opacity-0 z-0"
           } ${radius ? "rounded-3xl" : ""}`}
